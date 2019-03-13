d18ba69bb4b83bad680010643ee90cf3473d9e2b
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-13 15:20:03 -0700

    Add DataDao and code to test in MainActivity.

--- app/src/main/java/com/jbseppanen/android_kotlin_recyclerview/MainActivity.kt
+++ app/src/main/java/com/jbseppanen/android_kotlin_recyclerview/MainActivity.kt
@@ -2,11 +2,20 @@ package com.jbseppanen.android_kotlin_recyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DataDao.getDiversions(object : DataDao.DataCallback {
+            override fun callback(diversion: Diversion) {
+                runOnUiThread({findViewById<TextView>(R.id.test).setText(diversion.activity)})
+            }
+        })
+
+
     }
 }
--- app/src/main/java/com/jbseppanen/android_kotlin_recyclerview/Models.kt
+++ app/src/main/java/com/jbseppanen/android_kotlin_recyclerview/Models.kt
@@ -1,14 +1,15 @@
 package com.jbseppanen.android_kotlin_recyclerview
 
+import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SomethingToDo(
-    val accessibility: Double,
-    val activity: String,
-    val key: String,
-    val link: String,
-    val participants: Int,
-    val price: Double,
-    val type: String
+data class Diversion(
+    val accessibility: Double? = 0.0,
+    val activity: String? = "",
+    val key: String? = "",
+//    val link: String? = "",  //TODO handle the link when not present.
+    val participants: Int? = 0,
+    val price: Double? = 0.0,
+    val type: String? = ""
 )
