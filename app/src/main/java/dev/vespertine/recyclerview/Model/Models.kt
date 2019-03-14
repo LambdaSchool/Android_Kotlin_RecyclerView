25b6909f64ef6067bb344573038b144d3aee89e2
Patrick Martin <patrickmartin88@gmail.com>
2019-03-13 19:08:00 -0700

    Added DragQueen Model

---
+++ app/src/main/java/dev/vespertine/recyclerview/Model/Models.kt
@@ -0,0 +1,9 @@
+package dev.vespertine.recyclerview.Model
+
+
+data class DragQueen(val id: Number?,
+                          val name: String?,
+                          val winner: Boolean?,
+                          val missCongeniality: Boolean?,
+                          val image_url: String?,
+                          val quote: String?)
