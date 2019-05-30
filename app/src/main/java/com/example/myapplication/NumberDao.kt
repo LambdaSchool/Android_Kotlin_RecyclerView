0964f668ecbf048ab8635c2f4e888e0894f04e9c
BL-CoderOvertime <brandonlively@live.com>
2019-05-30 10:48:05 -0400

    Setup Model Class, started to define the Dao and listAdapter

---
+++ app/src/main/java/com/example/myapplication/NumberDao.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+
+object NumberDao{
+    const val NUMBERS_URL = "numbersapi.com/"
+    const val NUMBERS_URL_ENDING = "?json"
+
+
+
+}
--- app/src/main/java/com/example/myapplication/NumbersListAdapter.kt
+++ app/src/main/java/com/example/myapplication/NumbersListAdapter.kt
@@ -1,2 +1,36 @@
 package com.example.myapplication
 
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class NumbersListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    class NumberItemViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val numberView: TextView = view.findViewById(R.id.tv_number)
+        val factView: TextView = view.findViewById(R.id.tv_fact)
+    }
+
+    companion object {
+        const val ITEMS_PER_QUERY = 10
+    }
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+
+    }
+
+
+}
